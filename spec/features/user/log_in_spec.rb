b2f52fc96e32e44872527dc8fd3b0e6ebd3f5c99
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-02-21 02:34:09 +0300

    user can log in

---
+++ spec/features/user/log_in_spec.rb
@@ -0,0 +1,24 @@
+feature 'User can log in', %q{
+  I'd like to be able to log in
+  to create your own content and comment on other users, etc
+} do
+
+  given(:user) { create(:user) }
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'this@email.notfound'
+    fill_in 'Password', with: '0987654321'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
