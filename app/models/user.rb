0ac94f439a0eb645999a0c816e3e4d0cdff7b723
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-02-22 01:42:21 +0300

    added User dependent: :destroy for reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  has_many :reviews, foreign_key: 'author_id'
+  has_many :reviews, foreign_key: 'author_id', dependent: :destroy
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :username, presence: true, uniqueness: { case_sensitive: false }
