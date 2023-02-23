f5d5a5c289f43d235276e7e7861cae8f230061ae
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-02-23 21:57:53 +0300

    added user's dropmenu in header

--- spec/features/user/log_out_spec.rb
+++ spec/features/user/log_out_spec.rb
@@ -4,9 +4,10 @@ feature 'User can log out', %q(
   I want to log out
 ) do
 
-  scenario 'Authenticated user tries to log out' do
+  scenario 'Authenticated user tries to log out', js: true do
     log_in(create(:user))
 
+    find('#headUserMenu').click
     click_on 'Log out'
     expect(page).to have_content('Signed out successfully.')
   end
