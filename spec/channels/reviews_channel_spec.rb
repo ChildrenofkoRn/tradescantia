743a366d9b6820c1a48cb4cf819c265d7711d673
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-04-17 19:09:29 +0300

    updated reviews_channel_spec.rb

--- spec/channels/reviews_channel_spec.rb
+++ spec/channels/reviews_channel_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "successfully subscribes" do
+    subscribe
+
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_from('reviews')
+  end
 end
