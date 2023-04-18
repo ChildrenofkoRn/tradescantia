aa309127655f061c398919b6f4c350cb4903675f
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-04-18 16:04:42 +0300

    a simple /profiles/me is implemented

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -6,8 +6,6 @@ describe 'Profiles API', type: :request do
       'ACCEPT'       => 'application/json' }
   end
 
-  let(:api_path) { '/api/v1/profiles/me' }
-
   describe 'GET /api/v1/profiles/me' do
     let(:api_path) { '/api/v1/profiles/me' }
 
@@ -15,16 +13,43 @@ describe 'Profiles API', type: :request do
       it 'returns 401 status if there is no access_token' do
         get api_path, headers: headers
 
-        expect(response.status).to eq 401
+        expect(response).to be_unauthorized
       end
 
       it 'returns 401 status if access_token is invalid' do
         get api_path, params: { access_token: 'fc4bdc8be4' }, headers: headers
 
-        expect(response.status).to eq 401
+        expect(response).to be_unauthorized
       end
     end
 
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let(:token) { create(:api_token, resource_owner_id: me.id).token }
+
+      before do
+        get api_path,
+            params: { access_token: token },
+            headers: headers
+      end
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields' do
+        %w[id email created_at updated_at].each do |attr|
+          expect(json[attr]).to eq me.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
   end
 
 end
