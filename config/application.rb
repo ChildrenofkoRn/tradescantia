61ca6f85a5e83d09129876f3946cd4fbd9050c1c
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-05-26 01:08:41 +0300

    added custom pages for errors

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Tradescantia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.exceptions_app = ->(env) {
+      ErrorsController.action(:show).call(env)
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
