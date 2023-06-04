a44f03be1281fda09f91031123135e2e869b4676
ChildrenofkoRn <ChildrenofkoRn.PM@gmail.com>
2023-06-04 06:42:49 +0300

    registering via oauth: if username already, then its created through username and uid

--- spec/services/find_for_oauth_service_spec.rb
+++ spec/services/find_for_oauth_service_spec.rb
@@ -86,6 +86,47 @@ RSpec.describe FindForOauthService, type: :services do
       end
     end
 
+    context 'user doesnt exist, but username has already been taken' do
+      let(:username)  { 'rnd' }
+      let!(:another_user)  { create(:user, username: username) }
+      let(:auth) { OmniAuth::AuthHash.new(provider: 'github',
+                                          uid: '8282',
+                                          info: { email: "#{username}@mail.space" }) }
+
+      it 'creates new user' do
+        expect { subject }.to change(User, :count).by(1)
+      end
+
+      it 'returns new user' do
+        expect(subject).to be_a(User)
+      end
+
+      it 'fills user email' do
+        user = subject
+        expect(user.email).to eq auth.info[:email]
+      end
+
+      it 'fills user name' do
+        user = subject
+        expect(user.username).to eq "#{username}_#{auth.uid}"
+      end
+
+      it 'creates authorization' do
+        expect{subject}.to change(Authorization, :count).by(1)
+      end
+
+      it 'creates authorization for user' do
+        user = subject
+        expect(user.authorizations).to_not be_empty
+      end
+
+      it 'creates authorization with provider and uid' do
+        authorization = subject.authorizations.first
+        expect(authorization.provider).to eq auth.provider
+        expect(authorization.uid).to eq auth.uid
+      end
+    end
+
     context 'user does not exist and doesn\'t email' do
       let(:auth) { OmniAuth::AuthHash.new(provider: 'github',
                                           uid: '8282') }
